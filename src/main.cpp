dbe40f9ca80fd08b9bf8c3ef7ef6bb41c9a2ebcd
ctenosaura1 <240000240+ctenosaura1@users.noreply.github.com>
2026-08-20 22:04:09 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include <vk_engine.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[])
-{
-	VulkanEngine engine;
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
-	engine.init();
+#include <iostream>
 
-	engine.run();
+int main() {
+    glfwInit();
 
-	engine.cleanup();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-	return 0;
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
