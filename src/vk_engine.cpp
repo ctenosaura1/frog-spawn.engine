177a39f9762a5f7b1e6eb5e5b6843ae2b8991d5b
ctenosaura1 <240000240+ctenosaura1@users.noreply.github.com>
2026-08-22 13:46:34 +0800

    changed glfw to sdl3

---
+++ src/vk_engine.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vulkan/vulkan.h>
+
+VkApplicationInfo appInfo{
+    .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    .pApplicationName = "FrogSpawner",
+    .apiVersion = VK_API_VERSION_1_3
+};
